689777e32e147fe32728c38203b50890b1302f3d
jmodhiambo <jmodhiambo9346@gmail.com>
2025-05-13 17:48:32 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-""" UTF-8 Validation"""
+"""
+UTF-8 Validation
+"""
 
 
 def validUTF8(data) -> bool:
-    """Returns True is valid UTF-8 and False when otherwise."""
-    try:
-        bytes(data).decode('utf-8')
-        return True
-    except ValueError:
-        return False
+    """Check if data represents a valid UTF-8 encoding"""
+    n_bytes = 0
+
+    for num in data:
+        if num > 255:
+            return False
+
+        if n_bytes == 0:
+            if (num >> 5) == 0b110:
+                n_bytes = 1
+            elif (num >> 4) == 0b1110:
+                n_bytes = 2
+            elif (num >> 3) == 0b11110:
+                n_bytes = 3
+            elif (num >> 7):
+                return False
+        else:
+            if (num >> 6) != 0b10:
+                return False
+            n_bytes -= 1
+
+    return n_bytes == 0
