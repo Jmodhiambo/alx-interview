03b26cf1dc51001e2636ce02d9360eaf03e31490
jmodhiambo <jmodhiambo9346@gmail.com>
2025-06-02 15:53:29 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,37 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
 
 if (process.argv.length < 3) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>')
-  process.exit(1)
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
 }
 
-const movieId = process.argv[2]
-const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`
+const movieId = process.argv[2];
+const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 request(filmUrl, (error, response, body) => {
   if (error) {
-    console.error(error)
-    return
+    console.error(error);
+    return;
   }
 
-  const filmData = JSON.parse(body)
-  const characters = filmData.characters
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
 
   const printCharacter = (index) => {
-    if (index >= characters.length) return
+    if (index >= characters.length) return;
 
     request(characters[index], (err, res, charBody) => {
       if (!err) {
-        const character = JSON.parse(charBody)
-        console.log(character.name)
-        printCharacter(index + 1)
+        const character = JSON.parse(charBody);
+        console.log(character.name);
+        printCharacter(index + 1);
       } else {
-        console.error(err)
+        console.error(err);
       }
-    })
-  }
+    });
+  };
 
-  printCharacter(0)
-})
+  printCharacter(0);
+});
