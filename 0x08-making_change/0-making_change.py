bef34a87c3a44a0b0bfc13e856c505a342ea7a38
jmodhiambo <jmodhiambo9346@gmail.com>
2025-06-11 03:41:15 +0300

    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-"""Making Change."""
-
-from typing import List
+from typing import List, Union
 
 
 def makeChange(coins: List[int], total: int) -> int:
-    """Returns the fewest number of coins needed to meet total."""
+    """Returns the fewest number of coins needed to meet total using DP."""
     if total <= 0:
         return 0
 
-    count: int = 0
-    summation: int = 0
-    i: int = 0
+    dp: List[Union[int, float]] = [float('inf')] * (total + 1)
+    dp[0] = 0
 
-    coins.sort(reverse=True)
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if coin <= amount:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    while True:
-        if summation > total:
-            return -1
-        if summation == total:
-            return count
-
-        if (summation + coins[i]) > total:
-            i += 1
-
-        summation += coins[i]
-        count += 1
+    return int(dp[total]) if dp[total] != float('inf') else -1
